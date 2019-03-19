aa7da17a7c337540b5f06f7b4fb5ef81e16b4649
unknown <sontrng@BG1WS0405.SONATA.LOCAL>
2019-03-19 12:25:37 +0530

    2nd

---
+++ Test/com/sonata/CustomerRegisterTest.java
@@ -0,0 +1,26 @@
+package com.sonata;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+@SuppressWarnings("unused")
+public class CustomerRegisterTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDoPost() {
+		
+	}
+
+}
